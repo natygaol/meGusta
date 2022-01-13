ce7f7dd10ccd7c26ac1851130a4b0289bd6dd0c5
nataly garcia <naty_garcioli@hotmail.com>
2022-01-13 18:27:36 -0500

    aumenta likes en todos los divs

--- js/index.js
+++ js/index.js
@@ -1,11 +1,31 @@
-let counter = 3
+let counter = 9
 
-let likesCount = document.querySelector("#likes-total");
+let likesCount = document.querySelector("#likes-1");
 console.log(likesCount)
 
 function aumentaLikes ( elemento ) {
   counter ++;
   likesCount.innerText = counter + " like(s)";
   console.log(counter);
+
+}
+
+let counter2 = 12
+let likesCount2 = document.querySelector("#likes-2");
+
+function aumentaLikes2 ( elemento ) {
+  counter2 ++;
+  likesCount2.innerText = counter2 + " like(s)";
+  console.log(counter2);
+
+}
+
+let counter3 = 9
+let likesCount3 = document.querySelector("#likes-3");
+
+function aumentaLikes3 ( elemento ) {
+  counter3 ++;
+  likesCount3.innerText = counter3 + " like(s)";
+  console.log(counter3);
 
 }
